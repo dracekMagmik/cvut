e979846793f898cf86c88cab3163255fb448e9c8
matesiny <matesiny.tom@gmail.com>
2024-11-01 23:01:44 +0100

    hey. startn uloha 4 :')

--- algebrogram.py
+++ algebrogram.py
@@ -10,10 +10,16 @@ def readInput(name, array):
         else:
             break
 
+def unique(ar):
+    for a in ar:
+        for i in range(len(a)):
+            if ord(a[i]) > 64 and ord(a[i]) < 91:
+                letters.add(a[i])
+
+
 def replace(str, ch, op, eq): # op = index of operand, eq = index of equals
     new = str[eq+1:] + ch + str[op+1:eq] + '=' + str [:op]
     return new
-
 
 def plus_multiply(array):
     for i in range(len(array)):
@@ -31,10 +37,49 @@ def plus_multiply(array):
                 array[i] = replace(array[i], c, k, j)
     return array
 
-ar = []
+#mrkne na algebrogram a vrati velikosti cisel
+def chunk_sizes(str):
+    i = j = k = ''
+    index = 0
+    plus = True
+    for a in str:
+        if ord(a) > 64 and ord(a) < 91:
+            if index == 0: i = i + a
+            elif index == 1: j = j + a
+            elif index == 2: k = k + a
+        else:
+            if a == '*':
+                plus = False
+            index += 1
 
+    if plus:
+        if(third_largest(i,j,k)):
+            values[i[0]] = (1, True)
+
+
+
+def third_largest(i,j,k):
+    bigger = False
+    if len(i) < len(k) and len(j) < len(k):
+        return True
+    else:
+        return False
+
+
+letters = set()
+values = dict()
+ar = []
 readInput("algebrogram.txt", ar)
 ar = plus_multiply(ar)
 # all in good format and even only + and / operations :) nice
 
+unique(ar)
+
+print(letters)
+
+for a in ar:
+    chunk_sizes(a)
+
+
 print(ar)
+
