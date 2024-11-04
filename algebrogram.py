028a2cfe1f373fe0fc97f3f7c1dc8f84fa001045
matesiny <matesiny.tom@gmail.com>
2024-11-04 22:41:31 +0100

    hey. startn uloha 4 :')

--- algebrogram.py
+++ algebrogram.py
@@ -61,7 +61,7 @@ def plus_multiply(array):
     return array
 
 #mrkne na algebrogram a vrati velikosti cisel
-def chunk_sizes(str):
+def chunk_sizes(str, taken):
     i = j = k = ''
     index = 0
     plus = True
@@ -75,12 +75,137 @@ def chunk_sizes(str):
                 plus = False
             index += 1
 
+    # hodnoty na prvních místech nebudou nula
+    if 0 in values[i[0]][3] == False:
+        values[i[0]][3].append(0)
+
+    if 0 in values[j[0]][3] == False:
+        values[j[0]][3].append(0)
+
+    if 0 in values[k[0]][3] == False:
+        values[k[0]][3].append(0)
+
+    #algebrogramy? more like algebrolamy (im very sad)
+    # tohle se koukna na prvni mista v nasobeni a kdyz se vsechny cisla lisi, pak tak sčítanci(jemnuje se to tak, lol?)
+    # nejsou nula. podobny princip s jednickou v nasobeni
+    if i[-1] != k[-1] and j[-1] != k[-1]:
+        if plus:
+            if values[i[-1]][3] == False:
+                values[i[-1]][3].append(0)
+            if values[j[-1]][3] == False:
+                values[j[-1]][3].append(0)
+        else:
+            if values[i[-1]][3] == False:
+                values[i[-1]][3].append(0)
+                values[i[-1]][3].append(1)
+            if values[j[-1]][3] == False:
+                values[j[-1]][3].append(0)
+                values[j[-1]][3].append(1)
+
+
+
     if plus:
+        # ok, takže tady budu zkoušet ja nevimmmm, tak optimalizace dve a je to tyjo ugh....
+        # no jako, když je to třeba AB+CD=EB, tak potom D je 0, to asi hodim do toho if plus dole
+        if i[-1] == k[-1] and i[-1] != j[-1]:
+            if taken[0] and values[j[-1]][0] != 0:
+                return False
+            values[j[-1]][0] = 0
+            values[j[-1]][1] = True
+            taken[0] = True
+        elif j[-1] == k[-1] and i[-1] != j[-1]:
+            if taken[0] and values[i[-1]][0] != 0:
+                return False
+            values[i[-1]][0] = 0
+            values[i[-1]][1] = True
+            taken[0] = True
+
         if(third_largest(i,j,k)):
-            values[k[0]] = (1, True)
+            if taken[1] and values[k[0]][0] != 1:
+                return False
+            else:
+                values[k[0]][0] = 1
+                values[k[0]][1] = True
+                taken[1] = True
+    return True
+
+
+def re_chunk_size(str, taken):
+    i = j = k = ''
+    index = 0
+    plus = True
+    for a in str:
+        if ord(a) > 64 and ord(a) < 91:
+            if index == 0:
+                i = i + a
+            elif index == 1:
+                j = j + a
+            elif index == 2:
+                k = k + a
+        else:
+            if a == '*':
+                plus = False
+            index += 1
+
+    if plus:
+        if (third_largest(i, j, k)):
+            if taken[1] and values[k[0]][0] != 1:
+                return False
+            else:
+                values[k[0]][0] = 1
+                values[k[0]][1] = True
+                taken[1] = True
+
+            if values[k[1]][0] == 0:
+                # takže vysledek je treba 105, a to ze dvou dvoucif. cisel, takže A+B+1(cislo z i, j a preteceni) = 10. jinak se tam 0 nedostane.
+                # tim padem A+B+1 = 10 -> A+B = 9
+                if i[0] == k[1]:  # je to nula
+                    if taken[9] and values[j[0]][0] != 9:
+                        return False
+                    values[j[0]][0] = 9
+                    values[j[0]][1] = True
+                elif j[0] == k[1]:  # je to nula
+                    if taken[9] and values[i[0]][0] != 9:
+                        return False
+                    values[i[0]][0] = 9
+                    values[i[0]][1] = True
+
+                # tady uz to nemusi byt nutne s pretecenim 10+9s = 10s a 's+0 nepretece'
+                elif i[0] == k[0]:  # je to 1
+                    if not taken[8]:
+                        values[j[0]][2].append(8)
+                    if not taken[9]:
+                        values[j[0]][2].append(9)
+
+                elif j[0] == k[0]:  # je to 1
+                    if not taken[8]:
+                        values[i[0]][2].append(8)
+                    if not taken[9]:
+                        values[i[0]][2].append(9)
+
+                else:
+                    values[i[0]][3].append(8)
+                    values[i[0]][3].append(9)
+                    values[j[0]][3].append(8)
+                    values[j[0]][3].append(9)
+
+    #nejaka nasobeni optimalizace potreba :skull: ale COOOO!?!?!?!!!!! uz to mam, ale dal jsem to do puvodni chunks, nahoře
+    # to nestacilo. takze kdyby nahodou bylo posledni cislo v nasobku 0, a nebyla tam jinde nula, muselo to byt 5*sude cislo
+    else:
+        if values[k[-1]][0] == 0:
+            if 0 in values[i[-1]][3] and 0 in values[j[-1]][3]:
+                values[i[-1]][3].append(3)
+                values[i[-1]][3].append(7)
+                values[i[-1]][3].append(9)
+
+                values[j[-1]][3].append(3)
+                values[j[-1]][3].append(7)
+                values[j[-1]][3].append(9)
+
+    return True
+
 
 def third_largest(i,j,k):
-    bigger = False
     if len(i) < len(k) and len(j) < len(k):
         return True
     else:
@@ -88,17 +213,7 @@ def third_largest(i,j,k):
 
 def ini_dict(dict, letters):
     for i in letters:
-        dict[i] = [-1, False]
-
-def diff_vals(dict, letters, taken): # koukne, zda jsou vsechny lockly hodnoty jiny
-    for i in letters:
-        if (dict[i][1]):
-            if(taken[dict[i][0]] == False):
-                taken[dict[i][0]] = True
-                continue
-            else:
-                return False
-    return True
+        dict[i] = [-1, False, list(), list()]# me, going actually insane dude
 
 def try_values(dict, letters, index, taken):
     if(dict[letters[index]][1]): #hodnota je lockla
@@ -110,7 +225,24 @@ def try_values(dict, letters, index, taken):
                 return True
 
     else:
+        for n in dict[letters[index]][2]:
+            if taken[n] == False:
+                taken[n] = True
+                dict[letters[index]][0] = n
+
+                if index != len(letters) - 1:
+                    if try_values(dict, letters, index + 1, taken):
+                        return True
+
+                else:
+                    if dosad(dict):
+                        return True
+
+                taken[n] = False
+
         for i in range(10):
+            if i in dict[letters[index]][3] or i in dict[letters[index]][2]:
+                continue
             if taken[i] == False:
                 taken[i] = True
                 dict[letters[index]][0] = i
@@ -169,14 +301,19 @@ def dosad(dict):
 
 
 
+
+
 values = dict()
-
-'''ar = list()
-readInput("algebrogram.txt", ar)'''
-
 ar = []
-for l in sys.stdin:
-    ar.append(l.strip())
+
+'''input method'!!!!!!!!!!!!!!!!!!!!'''
+
+
+#readInput("algebrogram.txt", ar)
+
+for l in sys.stdin: ar.append(l.strip())
+
+
 
 ar = plus_multiply(ar)
 # all in good format and even only + and / operations :) nice
@@ -186,20 +323,23 @@ letters = unique(ar)
 # mam všechny pismena a to v abecednim poradi :)
 
 ini_dict(values, letters)
+taken = [False]*10
 
 for a in ar:
-    chunk_sizes(a)
+    if chunk_sizes(a, taken) == False:
+        print('NEEXISTUJE')
+        exit()
 
-# dve stejne lockle hodnoty
-taken = [False]*10
-if(diff_vals(values, letters, taken) == False):
-    print('NEEXISTUJE')
-    exit()
+for a in ar:
+    if re_chunk_size(a, taken) == False:
+        print('NEEXISTUJE')
+        exit()
 
 #print(values)
 #print(ar)
 
 ''' !!!!!!!!!!!!!!!!ok, takže nikdy nejsou na začátku nuly. + je třeba další optimatizace!!!!!!!!!!!!!!!!!!'''
+# no fakt dík. teĎ to musim dělat já a ty jsi si byl schrupnout.
 
 if try_values(values, letters, 0, taken):
     for i in range(len(letters)):
@@ -209,3 +349,4 @@ if try_values(values, letters, 0, taken):
             print(values[letters[i]][0], end = ' ')
 else:
     print('NEEXISTUJE')
+
